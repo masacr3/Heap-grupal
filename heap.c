b960cb0c8ac7df4e6f4e2aa9fa1b24dcbc3fdd0b
Leonel R <leorhcp2030@gmail.com>
2018-06-03 02:28:41 -0300

    fixed parrams

--- heap.c
+++ heap.c
@@ -1,14 +1,21 @@
+/*
+  Hola marto, tendriamos que  modificar el comportamiento del
+  vector dinamico para que agarre cosas void. por lo cual
+  tendriamos que Recodificar el como eliminamos los elementos
+  del vector dinamico
+*/
+
+
 #include "heap.h"
-#include "vector_dinamico.h"
 #include <string.h>
 
 #define TAMANIO_INICIAL :P //modify for work
 
 /*struct*/
 struct heap {
-	size_t cant;
-	size_t tam;
-	vector_t* vector;
+  size_t cantidad;
+  size_t capacidad;
+  void** vector;
   cmp_func_t cmp;
 };
 
@@ -17,30 +24,56 @@ heap_t *heap_crear(cmp_func_t cmp){
 
   if(!heap) return NULL;
 
-  vector_t *vector = vector_crear(TAMANIO_INICIAL);
+  void** vector = vector_crear(TAMANIO_INICIAL);
 
   if(!vector){
     free(heap);
     return NULL;
   }
 
-  heap->cant = 0;
-  heap->tam = TAMANIO_INICIAL;
+  heap->cantidad = 0;
+  heap->capacidad = TAMANIO_INICIAL;
   heap->cmp = cmp;
 
   return heap;
 }
 
 size_t heap_cantidad(const heap_t *heap){
-  return heap->cant;
+  return heap->cantidad;
 }
 
 void *heap_ver_max(const heap_t *heap){
-  if ( heap->cant == 0 ) return NULL;
+  if ( heap->cantidad == 0 ) return NULL;
 
   return vector_obtener(heap->vector, 0) // el maximo se encuentra en la posicion maxima del vector
 }
 
 bool heap_esta_vacio(const heap_t *heap){
-  return heap->cant == 0;
+  return heap->cantidad == 0;
 }
+
+
+
+bool heap_encolar(heap_t *heap, void *elem){
+	if (!elem) return false;
+
+  if (heap->cantidad == heap->capacidad ) redimensionar () // <--- redimensionar
+
+  // agrego el elemento al arreglo
+  // despues aplico upheap para que lo acomode solo
+  heap->array[ heap->cantidad ] = elem;
+  heap->cantidad++;
+
+  upheap(heap);
+	return true;
+}
+
+/*
+
+        padre = (pos - 1) / 2
+        ultipadre = (len - 1 / 2);
+
+        hijo-izq = 2 * pos + 1
+        hijo-der = 2 * pos + 2
+
+*/
