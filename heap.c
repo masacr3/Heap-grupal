158bd80f175bed1491745f742e556939bde6aaa2
Leonel R <leorhcp2030@gmail.com>
2018-06-04 23:02:57 -0300

    add downheap and REORGANIZE code

--- heap.c
+++ heap.c
@@ -1,8 +1,13 @@
 /*
-  Hola marto, tendriamos que  modificar el comportamiento del
-  vector dinamico para que agarre cosas void. por lo cual
-  tendriamos que Recodificar el como eliminamos los elementos
-  del vector dinamico
+  Hola marto, te comento que hay que organizar mas el codigo
+  en el header tendriamos q poner la deficion de los prototipos
+  auxiliares.
+
+  te comento q las funciones faltan optimizarlas. como el tema de redimensionar
+  el factor de carga, no lo tengo .. solo tengo lo q me diste de teoria
+
+  cabe destacar q no vamos a usar vector_dinamico.h pero vamos a tomar bastantes ideas de maximo_hijo
+  Bueno Exitos para fisica :P
 */
 
 
@@ -18,6 +23,18 @@ struct heap {
   void** vector;
   cmp_func_t cmp;
 };
+
+/* prototipos funciones auxiliares */
+
+//retorna NULL en caso que este fuera de rango
+void* obtener(void** array, size_t*pos);
+
+//retorna la posicion max
+//pre: valor1 y valor 2 son posiciones validas
+size_t* obtener_max(void** array, size_t valor1, size_t valor2);
+
+
+
 
 heap_t *heap_crear(cmp_func_t cmp){
   heap_t* heap = malloc( sizeof(heap_t) );
@@ -81,8 +98,37 @@ bool heap_encolar(heap_t *heap, void *elem){
 	return true;
 }
 
-/*
+void downheap(void** array, size_t cantidad, size_t pibote ){
+  if (pibote >= cantidad) return ;
 
+  size_t pos_hijo_derecho = 2 * pibote + 1;
+  size_t pos_hijo_izquierdo = 2 * pibote + 2;
+
+  void* hijo_derecho = obtener(array, pos_hijo_derecho);
+  void* hijo_izquierdo = obtener(array, pos_hijo_izquierdo);
+
+  //termino de acomodar
+  if ( !hijo_izquierdo && !hijo_derecho ) return ;
+
+  //es por que existen los 2 hijos
+  if (hijo_derecho){
+    //calculo cual es el maximo
+    size_t maximo_hijo = obtener_max( hijo_izquierdo,hijo_derecho);
+
+    //cambio posiciones
+    swap(array[pibote],array[maximo_hijo]);
+
+    downheap(array,cantidad,maximo_hijo);
+  }
+
+  //si cai aca es por que solo existe el hijo derecho
+  swap(array[pibote],array[pos_hijo_izquierdo]);
+
+  downheap(array,cantidad,pos_hijo_izquierdo);
+}
+
+/*
+        MACHETE :P 
         padre = (pos - 1) / 2
         ultipadre = (len - 1 / 2);
 
